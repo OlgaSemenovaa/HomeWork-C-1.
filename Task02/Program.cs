29c849b84228d83f97fb611dbbd23ee83175721f
Olga Semenova <olya.meoow@yandex.ru>
2022-10-20 13:18:12 +0300

    changes in Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -13,8 +13,10 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
-{
-Console.WriteLine($"a = max");
-}
-else Console.WriteLine ("b = max");
+int max = a;
+
+if (a > b) max = a;
+if (b > a) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
