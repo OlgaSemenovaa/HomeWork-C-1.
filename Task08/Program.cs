e5d53b79da8f71d71d7fdf891932d022c8218626
Olga Semenova <olya.meoow@yandex.ru>
2022-10-20 14:39:54 +0300

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+// Пример:
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine ("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 1)
+Console.WriteLine("Недопустимое число");
+else
+{
+    int count = a%2;
+    while (count <= a)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+}
